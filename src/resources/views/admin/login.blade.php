e266708c267acb12a1b3c0d4d2c92e990a0a72e9
ichiki925 <spectafilm925@gmail.com>
2025-02-04 03:28:14 +0900

    管理者ログインview

---
+++ src/resources/views/admin/login.blade.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>管理者ログイン</title>
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+    <link rel="stylesheet" href="{{ asset('css/admin/login.css') }}" />
+
+</head>
+<body>
+    <header class="header">
+        <div class="container">
+            <div class="logo">
+                <img src="{{ asset('images/logo.svg') }}" alt="Logo">
+            </div>
+        </div>
+    </header>
+
+    <main class="form-container">
+        <h1 class="login-title">管理者ログイン</h1>
+
+        <form action="#" method="POST">
+            <div class="form-group">
+                <label for="email" class="form-label">メールアドレス</label>
+                <input type="email" id="email" name="email" class="form-input" required>
+            </div>
+
+            <div class="form-group">
+                <label for="password" class="form-label">パスワード</label>
+                <input type="password" id="password" name="password" class="form-input" required>
+            </div>
+
+            <button type="submit" class="login-button">管理者ログインする</button>
+        </form>
+    </main>
+</body>
+</html>
